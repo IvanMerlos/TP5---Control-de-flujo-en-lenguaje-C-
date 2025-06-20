c6ebbd60904e8cce98dd9ff77a27c0f5e30aea76
Ivan Mateo Merlos <merlosivan24@gmail.com>
2025-06-20 19:50:55 -0300

    Add files via upload

---
+++ Tp5-Control de flujo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int n;
+float nota,max,min,promedio = 0;
+int main()
+{
+ 
+    printf(" Ingrese c/de notas: ");
+    scanf("%d",&n);
+    
+    while(n<0){
+    printf(" Ingrese c/de notas *POSITIVAS*: ");
+    scanf("%d",&n);
+    }
+    
+for(int i=1;i<=n;i++){
+  printf(" Ingrese nota del alumno:");
+    scanf("%f",&nota);
+    
+    while(nota<0 || nota>100){
+     printf(" ERROR: Ingrese nota de 0 a 100\n");
+     printf(" Ingrese nota del alumno:");
+     scanf("%f",&nota);
+    }
+     
+      if (i == 1) {
+        max = min = nota;
+    }
+    else {
+        if (nota > max){
+         max = nota;
+        } 
+        if (nota < min){
+         min = nota;
+        } 
+    }
+
+    promedio += nota;
+}
+        float resultado = promedio/n;
+        printf(" La nota menor es %.2f y la mayor %.2f\n ",min,max);
+        printf(" El promedio es %.2f\n",resultado);
+    return 0;
+}
